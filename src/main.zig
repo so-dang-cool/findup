754ba8b65f1b2221739d450a220949834559d0cf
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-07-11 00:30:51 -0700

    add a fileExists function

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,30 @@
 const std = @import("std");
+const log = std.log;
+const Dir = std.fs.Dir;
+const AccessError = std.os.AccessError;
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    const cwd = std.fs.cwd();
+    log.info("CWD: {s}", .{cwd});
+
+    //try std.os.chdir("..");
+
+    inline for (.{ "build.zig", "nonexit.zig" }) |f| {
+        log.info("file: {s}", .{f});
+        if (try fileExists(cwd, f)) {
+            log.info("exists!", .{});
+        } else {
+            log.info("doesn't exist!", .{});
+        }
+    }
+}
+
+fn fileExists(dir: Dir, filename: []const u8) AccessError!bool {
+    dir.access(filename, .{}) catch |err| {
+        return switch (err) {
+            AccessError.FileNotFound => false,
+            else => err,
+        };
+    };
+    return true;
 }
