b797e28b39bf29cae554ca4e24d4211e5cc957c6
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-07-11 02:32:22 -0700

    add a printDir function and some args parsing

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,39 @@
 const std = @import("std");
-const log = std.log;
+const stdout = std.io.getStdOut().writer();
+const Writer = std.io.Writer;
 const Dir = std.fs.Dir;
+const MAX_PATH_BYTES = std.fs.MAX_PATH_BYTES;
 const AccessError = std.os.AccessError;
 
 pub fn main() anyerror!void {
-    const cwd = std.fs.cwd();
-    log.info("CWD: {s}", .{cwd});
+    var args = std.process.args();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    var allocator = &arena.allocator;
+    const program = try args.next(allocator).?;
+    try stdout.print("program: {s}\n", .{program});
+    const target = try args.next(allocator).?;
+    try stdout.print("target: {s}\n", .{target});
 
+    const cwd = std.fs.cwd();
+
+    try printDir(cwd);
     //try std.os.chdir("..");
 
-    inline for (.{ "build.zig", "nonexit.zig" }) |f| {
-        log.info("file: {s}", .{f});
+    inline for (.{ "build.zig", "nonexist" }) |f| {
+        try stdout.print("file: {s}\n", .{f});
         if (try fileExists(cwd, f)) {
-            log.info("exists!", .{});
+            try stdout.print("exists!\n", .{});
         } else {
-            log.info("doesn't exist!", .{});
+            try stdout.print("doesn't exist!\n", .{});
         }
     }
+}
+
+fn printDir(dir: Dir) anyerror!void {
+    var buffer: [MAX_PATH_BYTES]u8 = undefined;
+    const dir_str = try dir.realpath(".", &buffer);
+    try stdout.print("{s}\n", .{dir_str});
 }
 
 fn fileExists(dir: Dir, filename: []const u8) AccessError!bool {
