8d203474c06ddb74473df07ab28b243b7b58781f
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-02-27 21:29:18 -0800

    Add some bats tests

--- build.zig
+++ build.zig
@@ -1,20 +1,16 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+
+    // ----- install ------
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
-
     const exe = b.addExecutable("findup", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
+
+    // ----- run -----
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
@@ -24,4 +20,11 @@ pub fn build(b: *std.build.Builder) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    // ----- test -----
+
+    const test_cmd = b.addSystemCommand(&[_][]const u8{"./test.bats"});
+    test_cmd.step.dependOn(b.getInstallStep());
+    const test_step = b.step("test", "Run BATS to test the CLI");
+    test_step.dependOn(&test_cmd.step);
 }
